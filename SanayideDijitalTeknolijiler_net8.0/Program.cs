0b05b189536eb266442918a5bbf273f20e5a6e30
async_0 <static4sync@gmail.com>
2024-07-02 17:00:52 +0300

    tcp listener

--- SanayideDijitalTeknolijiler_net8.0/Program.cs
+++ SanayideDijitalTeknolijiler_net8.0/Program.cs
@@ -7,22 +7,9 @@ namespace GpioLedExample
     {
         static void Main(string[] args)
         {
-            // GPIO pinini tanımlayın (örneğin GPIO 17)
-            const int ledPin = 17;
-
-            // GPIO kontrol nesnesini oluşturun
-            using var gpio = new GpioController();
-
-            // Pin konfigürasyonunu yapın
-            gpio.OpenPin(ledPin, PinMode.Output);
-
-            // LED'i aç ve kapat
-            Console.WriteLine("LED yanıyor...");
-            gpio.Write(ledPin, PinValue.High);
-            System.Threading.Thread.Sleep(1000); // 1 saniye bekle
-
-            // Uygulamayı sonlandır
-            Console.WriteLine("Program sonlandı.");
+            Console.WriteLine("Program begin");
+            TcpServer server = new TcpServer(2804);
+            server.Listen();
         }
     }
 }
