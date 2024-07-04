4702f03db1e1fc9f0bb8ba4878d0da14381fed37
async_0 <static4sync@gmail.com>
2024-07-04 17:29:21 +0300

    Update Program.cs

--- SanayideDijitalTeknolijiler_net8.0/Program.cs
+++ SanayideDijitalTeknolijiler_net8.0/Program.cs
@@ -21,15 +21,17 @@ namespace SanayideDijitalTeknolijiler_net8._0
             LogSys.WarnLog("gergin ferhat");
             LogSys.ErrorLog("kızgın ferhat");
             LogSys.GrayLog("-------------------color board-------------------");
+
+            //MOTOR DENEME
             GC.PreparePin(2,PinMode.Output);
-            for(int i=0;i<10;i++){
-                GC.Write(2,PinValue.High);
-                Thread.Sleep(700);
-                GC.Write(2,PinValue.Low);
-            }
-            LogSys.WarnLog("PİN TAMAMEN KAPANIYOR AMK !!!");
-            GC.ClosePin(2);
-            
+            GC.PreparePin(3,PinMode.Output);
+            GC.PreparePin(20,PinMode.Output);
+            GC.PreparePin(21,PinMode.Output);
+            GC.Write(2,PinValue.High);
+            GC.Write(3,PinValue.Low);
+            GC.Write(20,PinValue.High);
+            GC.Write(21,PinValue.Low);
+            LogSys.InfoLog("motorlar calısıy");
 
         }
     }
