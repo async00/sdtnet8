95e13564904e22634dd9076d56e2142ee8152433
async_0 <static4sync@gmail.com>
2024-07-04 17:18:16 +0300

    Update Program.cs

--- SanayideDijitalTeknolijiler_net8.0/Program.cs
+++ SanayideDijitalTeknolijiler_net8.0/Program.cs
@@ -16,14 +16,20 @@ namespace SanayideDijitalTeknolijiler_net8._0
 
             //renk denemesi
             LogSys.GrayLog("-------------------color board-------------------");
-            
             LogSys.SuccesLog("mutlu ferhat");
             LogSys.InfoLog("düsünceli ferhat");
             LogSys.WarnLog("gergin ferhat");
             LogSys.ErrorLog("kızgın ferhat");
             LogSys.GrayLog("-------------------color board-------------------");
             GC.PreparePin(2,PinMode.Output);
-            GC.Write(2,PinValue.High);
+            for(int i=0;i<10;i++){
+                GC.Write(2,PinValue.High);
+                Thread.Sleep(700);
+                GC.Write(2,PinValue.Low);
+            }
+            LogSys.WarnLog("PİN TAMAMEN KAPANIYOR AMK !!!");
+            GC.ClosePin(2);
+            
 
         }
     }
