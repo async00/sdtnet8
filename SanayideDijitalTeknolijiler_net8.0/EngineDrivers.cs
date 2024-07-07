04fa79e21001c7b5c79fe81fc0be435af4835a9d
async_0 <static4sync@gmail.com>
2024-07-07 16:42:24 +0300

    Update EngineDrivers.cs
    yeni pinler

--- SanayideDijitalTeknolijiler_net8.0/EngineDrivers.cs
+++ SanayideDijitalTeknolijiler_net8.0/EngineDrivers.cs
@@ -10,54 +10,59 @@ namespace SanayideDijitalTeknolijiler_net8._0
     internal class EngineDrivers
     {
         //tcpden gelen değere göre veri girme
+        //6 13 19 26
+        private static int pin1=6;
+        private static int pin2=13;
+        private static int pin3=19;
+        private static int pin4=26;
         public static void Preapare_Engine_Pins(){
             //motor 1
-            GC.PreparePin(2,PinMode.Output);
-            GC.PreparePin(3,PinMode.Output);
+            GC.PreparePin(pin1,PinMode.Output);
+            GC.PreparePin(pin2,PinMode.Output);
             //motor 2
-            GC.PreparePin(20,PinMode.Output);
-            GC.PreparePin(21,PinMode.Output);
+            GC.PreparePin(pin3,PinMode.Output);
+            GC.PreparePin(pin4,PinMode.Output);
         }
         public static void Engine_FORWARD()
         {
-            GC.Write(2,PinValue.High);
-            GC.Write(3,PinValue.Low);
-            GC.Write(20,PinValue.High);
-            GC.Write(21,PinValue.Low);
+            GC.Write(pin1,PinValue.High);
+            GC.Write(pin2,PinValue.Low);
+            GC.Write(pin3,PinValue.High);
+            GC.Write(pin4,PinValue.Low);
         }
         public static void Engine_LEFT()
         {
             //1.motor ileri
-            GC.Write(2,PinValue.High);
-            GC.Write(3,PinValue.Low);
+            GC.Write(pin1,PinValue.High);
+            GC.Write(pin2,PinValue.Low);
 
             //2.motor geri
-            GC.Write(20,PinValue.Low);
-            GC.Write(21,PinValue.High);
+            GC.Write(pin3,PinValue.Low);
+            GC.Write(pin4,PinValue.High);
         }
         public static void Engine_RIGHT()
         {
         //1.motor geri
-            GC.Write(2,PinValue.Low);
-            GC.Write(3,PinValue.High);
+            GC.Write(pin1,PinValue.Low);
+            GC.Write(pin2,PinValue.High);
 
             //2.motor ileri
-            GC.Write(20,PinValue.High);
-            GC.Write(21,PinValue.Low);
+            GC.Write(pin3,PinValue.High);
+            GC.Write(pin4,PinValue.Low);
         }
         public static void Engine_BACKWARD()
         {
-            GC.Write(2,PinValue.Low);
-            GC.Write(3,PinValue.High);
-            GC.Write(20,PinValue.Low);
-            GC.Write(21,PinValue.High);
+            GC.Write(pin1,PinValue.Low);
+            GC.Write(pin2,PinValue.High);
+            GC.Write(pin3,PinValue.Low);
+            GC.Write(pin4,PinValue.High);
         }
         public static void Engine_RESET()
         {
-            GC.Write(2,PinValue.Low);
-            GC.Write(3,PinValue.Low);
-            GC.Write(20,PinValue.Low);
-            GC.Write(21,PinValue.Low);
+            GC.Write(pin1,PinValue.Low);
+            GC.Write(pin2,PinValue.Low);
+            GC.Write(pin3,PinValue.Low);
+            GC.Write(pin4,PinValue.Low);
         }
 
     }
