e0ad67680de892af6c485975e1ac80196161abb7
async_0 <static4sync@gmail.com>
2024-08-01 23:29:40 +0300

    Update PwmVirtualMode.cs

--- SanayideDijitalTeknolijiler_net8.0/PwmVirtualMode.cs
+++ SanayideDijitalTeknolijiler_net8.0/PwmVirtualMode.cs
@@ -34,8 +34,8 @@ namespace SanayideDijitalTeknolijiler_net8._0
                     GC.Write(3,PinValue.High);
                     GC.Write(17,PinValue.High);
                     GC.Write(27,PinValue.High);
-                    vpwm14.SetPercent(100);
                     vpwm14.SetPercent(0);
+                    vpwm14.SetPercent(100);
                     pwmclass12.SetDutyCycle(255);
                     pwmclass19.SetDutyCycle(0);
                     pwmclass12.Start();
@@ -77,8 +77,8 @@ namespace SanayideDijitalTeknolijiler_net8._0
                   
                     //12 SA GMOTOR ILERI
                     //13 SAG MOTOER GERI 
-                    vpwm14.SetPercent(0);
-                    vpwm15.SetPercent(100);
+                    vpwm14.SetPercent(100);
+                    vpwm15.SetPercent(0);
                     pwmclass12.SetDutyCycle(0);
                     pwmclass19.SetDutyCycle(255);
                     pwmclass12.Start();
